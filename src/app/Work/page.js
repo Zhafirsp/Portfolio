de956d3e28c241926e6d9b7e4d6c459b1a02c283
Zhafirsp <zhafirsp@gmail.com>
2024-12-03 11:05:28 +0700

    fix: projects import

--- src/app/Work/page.js
+++ src/app/Work/page.js
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import SlidingImages from "../../components/SlidingImages";
 import ParallaxScroll from "../../components/ParallaxScroll";
-import Projects from "../Projects";
+import Projects from "../projects";
 import styles from "./style.module.scss";
 import Transition from "../../components/Transition"; // Import the transition component
 import Contact from "../../components/Contact";
